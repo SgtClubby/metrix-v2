9c85289bf388dbfda591be9f2c212dc827dcb2f3
SgtClubby <assassin12@hotmail.no>
2021-11-06 14:03:19 +0100

    Command handler - BROKEN - Needs Fix

--- index.js
+++ index.js
@@ -6,49 +6,77 @@ const client = new Discord.Client()
 const config = require('./config.json')
 
 //Initialisation
-const fs = require('fs')
+const { readdirSync } = require("fs");
 const path = require('path')
 const chalk = require('chalk')
 
-//Loads all commands and aliases
-const loadCommands = (dir) => {
-    log("Loading all commands...")
-    fs.readdir(dir, (err, files) => {
-        if (err) return console.error(err)
-        files.forEach(file => {
-            if (!file.endsWith('.js')) return
-            const command = require(path.join(dir, file))
-            bot.commands.set(command.name, command)
-            if (command.aliases) command.aliases.forEach(alias => bot.aliases.set(alias, command.name))
-        })
-    })
-}
-
-//Loads all events
-const loadEvents = (dir) => {
-    log("Loading all events...")
-    fs.readdir(dir, (err, files) => {
-        if (err) return console.error(err)
-        files.forEach(file => {
-            const event = require(path.join(dir, file))
-            let eventName = file.split('.')[0]
-            bot.on(eventName, event.bind(null, bot))
-            delete require.cache[require.resolve(`${dir}/${file}`)]
-        })
-    })
-}
-
+//Define the maps
 client.commands = new Discord.Collection()
 client.aliases = new Discord.Collection()
 
-loadCommands('./commands')
-loadEvents('./events')
+
+//Loads all commands
+const loadCommands = async () => {
+    const commands = readdirSync("./commands/").filter(file => file.endsWith(".js"));
+    for (const file of commands) {
+      const command = require(`./commands/${file}`);
+      log(`Loading Command: ${command.info.name}`);
+      client.commands.set(command.info.name, command);
+      command.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, command.name);
+      });
+    }
+}
+loadCommands()
+
+// //Loads all events
+// const loadEvents = (dir) => {
+//     log("Loading all events...")
+//     fs.readdir(dir, (err, files) => {
+//         if (err) return console.error(err)
+//         files.forEach(file => {
+//             const event = require(path.join(dir, file))
+//             let eventName = file.split('.')[0]
+//             client.on(eventName, event.bind(null, bot))
+//             delete require.cache[require.resolve(`${dir}/${file}`)]
+//         })
+//     })
+// }
+// loadEvents('/home/assas/metrix-v2/events')
 
 client.login(config.token);
 
 client.on('ready', () => {
     log(`Logged in as ${client.user.tag}!`);
-    log(`Invite: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+    //log(`Invite: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
     client.user.setActivity('Bruh', { type: 'PLAYING' });
 });
 
+client.on('message', message => {
+    // message.content = message.content.toLowerCase()
+    log(message.content)
+    if (message.author.bot) return
+    if (message.channel.type === 'dm') return
+    if (message.content.startsWith(config.prefix)) {
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+        log(args)
+        const command = args.shift().toLowerCase()
+        log(command)
+
+        if (client.commands.has(command)) {
+            try {
+                client.commands.get(command).execute(message, args)
+            } catch (error) {
+                console.error(error)
+                message.reply('there was an error trying to execute that command!')
+            }
+        } else if (client.aliases.has(command)) {
+            try {
+                client.commands.get(client.aliases.get(command)).execute(message, args)
+            } catch (error) {
+                console.error(error)
+                message.reply('there was an error trying to execute that command!')
+            }
+        }
+    }
+});

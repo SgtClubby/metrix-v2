2fcaaecc9c418ae44b2c1675b7997dfc31027f72
SgtClubby <assassin12@hotmail.no>
2021-11-07 22:38:29 +0100

    Fixed command handler

--- index.js
+++ index.js
@@ -21,58 +21,51 @@ const loadCommands = async () => {
     for (const file of commands) {
       const command = require(`./commands/${file}`);
       log(`Loading Command: ${command.info.name}`);
-      client.commands.set(command.info.name, command);
+      client.commands.set(command.info.name.toLowerCase(), command);
       command.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, command.name);
+        client.aliases.set(alias, command.info.name);
       });
     }
 }
 loadCommands()
 
-// //Loads all events
-// const loadEvents = (dir) => {
-//     log("Loading all events...")
-//     fs.readdir(dir, (err, files) => {
-//         if (err) return console.error(err)
-//         files.forEach(file => {
-//             const event = require(path.join(dir, file))
-//             let eventName = file.split('.')[0]
-//             client.on(eventName, event.bind(null, bot))
-//             delete require.cache[require.resolve(`${dir}/${file}`)]
-//         })
-//     })
-// }
-// loadEvents('/home/assas/metrix-v2/events')
+const eventFiles = readdirSync("./events/").filter(file => file.endsWith(".js"));
+  for (const file of eventFiles) {
+    const eventName = file.split(".")[0];
+    log(`Loading Event: ${eventName}`);
+    const event = require(`./events/${file}`);
+    // Bind the client to any event, before the existing arguments
+    // provided by the discord.js event. 
+    // This line is awesome by the way. Just sayin'.
+    client.on(eventName, event.bind(null, client));
+  }  
 
 client.login(config.token);
 
 client.on('ready', () => {
     log(`Logged in as ${client.user.tag}!`);
-    //log(`Invite: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
+    log(`Invite: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot`)
     client.user.setActivity('Bruh', { type: 'PLAYING' });
 });
 
 client.on('message', message => {
-    // message.content = message.content.toLowerCase()
-    log(message.content)
+    message.content = message.content.toLowerCase()
     if (message.author.bot) return
     if (message.channel.type === 'dm') return
     if (message.content.startsWith(config.prefix)) {
         const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
-        log(args)
         const command = args.shift().toLowerCase()
-        log(command)
-
         if (client.commands.has(command)) {
             try {
-                client.commands.get(command).execute(message, args)
+                client.commands.get(command).run(client, message, args)
             } catch (error) {
                 console.error(error)
                 message.reply('there was an error trying to execute that command!')
             }
         } else if (client.aliases.has(command)) {
+            log(client.aliases.has(command))
             try {
-                client.commands.get(client.aliases.get(command)).execute(message, args)
+                client.commands.get(client.aliases.get(command)).run(client, message, args)
             } catch (error) {
                 console.error(error)
                 message.reply('there was an error trying to execute that command!')
